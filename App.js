a8b38e21607b54967c1c3b9ab06441a487a8de69
manuelsanchez2 <manusansan22@gmail.com>
2020-10-27 17:35:05 +0100

    Import fonts and create font loader

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
+  const [fontLoaded, setFontloaded] = useState(false);
+
+  if (!fontLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setFontloaded(true)}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
     </View>
   );
 }
@@ -15,5 +36,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  text: {
+    fontFamily: "open-sans-bold",
   },
 });
